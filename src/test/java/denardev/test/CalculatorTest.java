b6d88fef3b402ed94de3da6c38f30358d2a9fb8b
Denar_Ahmaron <denarahmaron1@gmail.com>
2025-03-07 23:18:10 +0700

    add display name test

--- src/test/java/denardev/test/CalculatorTest.java
+++ src/test/java/denardev/test/CalculatorTest.java
@@ -1,24 +1,30 @@
 package denardev.test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Test for calculator class")
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
 
     @Test
+    @DisplayName("Test for success scenario for method add(Integer, Integer)")
     public void testAddSuccess(){
         var result = calculator.add(1,1);
         assertEquals(2, result);
     }
 
     @Test
+    @DisplayName("Test for success scenario for method divide(Integer, Integer)")
     public void testDivideSuccess(){
         var result = calculator.divide(10, 2);
         assertEquals(5, result);
     }
 
     @Test
+    @DisplayName("Test for failed scenario for method divide(Integer, Integer)")
     public void testDivideFailed(){
         assertThrows(IllegalArgumentException.class, () -> {
             calculator.divide(10, 0);
