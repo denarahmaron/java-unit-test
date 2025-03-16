9ccfb35ad128b75910c10d9a097966c249fa7f98
Denar_Ahmaron <denarahmaron1@gmail.com>
2025-03-16 14:04:16 +0700

    test lifecycle

--- src/test/java/denardev/test/OrderedTest.java
+++ src/test/java/denardev/test/OrderedTest.java
@@ -1,28 +1,41 @@
 package denardev.test;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 public class OrderedTest {
+
+    public int counter;
+
+    @BeforeAll
+    void beforeAll() {
+
+    }
+
+    @AfterAll
+    void afterAll() {
+
+    }
 
     @Test
     @Order(1)
     public void testOrder3(){
-
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(2)
     public void testOrder2(){
-
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(3)
     public void testOrder1(){
-
+        counter++;
+        System.out.println(counter);
     }
 }
