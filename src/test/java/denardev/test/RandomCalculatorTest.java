06f8a4638e469253c5b87ec41c39987f2113710b
Denar_Ahmaron <denarahmaron1@gmail.com>
2025-03-24 22:39:42 +0700

    Repeated Test

--- src/test/java/denardev/test/RandomCalculatorTest.java
+++ src/test/java/denardev/test/RandomCalculatorTest.java
@@ -1,7 +1,6 @@
 package denardev.test;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.Extensions;
 import resolver.RandomParameterResolver;
@@ -17,6 +16,32 @@ public class RandomCalculatorTest {
 
     @Test
     void testRandom(Random random){
+        var a = random.nextInt();
+        var b = random.nextInt();
+        var result = calculator.add(a,b);
+        var expected = a + b;
+
+        Assertions.assertEquals(expected, result);
+    };
+
+    @DisplayName("Test random calculator")
+    @RepeatedTest(
+            value = 10,
+            name = "{displayName} ke {currentRepetition} dari {totalRepetitions}"
+    )
+    void testRandomRepeat(Random random){
+        var a = random.nextInt();
+        var b = random.nextInt();
+        var result = calculator.add(a,b);
+        var expected = a + b;
+
+        Assertions.assertEquals(expected, result);
+    };
+
+    @DisplayName("Test random calculator info")
+    @RepeatedTest(value = 10, name = "{displayName}")
+    void testRandomRepeatInfo(TestInfo info, Random random, RepetitionInfo repetitionInfo){
+        System.out.println(info.getDisplayName() + " ke " + repetitionInfo.getCurrentRepetition() + " dari " + repetitionInfo.getTotalRepetitions());
         var a = random.nextInt();
         var b = random.nextInt();
         var result = calculator.add(a,b);
