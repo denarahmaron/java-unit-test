a34bea6f21655a4ec9ed68ff49491a1a5fad5fce
Denar_Ahmaron <denarahmaron1@gmail.com>
2025-04-04 19:43:16 +0700

    parallel test

--- src/test/java/denardev/test/SlowTest.java
+++ src/test/java/denardev/test/SlowTest.java
@@ -2,14 +2,29 @@ package denardev.test;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.util.concurrent.TimeUnit;
 
+@Execution(value = ExecutionMode.CONCURRENT)
 public class SlowTest {
 
     @Test
     @Timeout(value = 5, unit = TimeUnit.SECONDS)
-    void slowTest() throws InterruptedException {
-        Thread.sleep(10_000);
+    void slowTest1() throws InterruptedException {
+        Thread.sleep(4_000);
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void slowTest2() throws InterruptedException {
+        Thread.sleep(4_000);
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void slowTest3() throws InterruptedException {
+        Thread.sleep(4_000);
     }
 }
